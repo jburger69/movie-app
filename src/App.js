2a63c47fac4c2ad5e65db770c8aea2af95ea2ec5
Joshua Burger <joshberger6969@outlook.com>
2022-06-22 22:09:04 -0400

    Create async function get movie request to grab specific movies passed in by searchValue if there is a search then set the movies, Create useEffect to initialize getMovieRequest function with searchValue passed in then set array with search value

--- src/App.js
+++ src/App.js
@@ -11,6 +11,19 @@ function App() {
   const [searchValue, setSearchValue] = useState('');
 
 
+  const getMovieRequest = async (searchValue) => {
+    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=6c8de510`
+    const response = await fetch(url);
+    const responseJson = await response.json();
+
+    if (responseJson.Search) {
+      setMovies(responseJson.Search)
+    }
+  };
+
+  useEffect(() => {
+    getMovieRequest(searchValue);
+  }, [searchValue])
 
 
   return (
