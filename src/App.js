ac6aabb011afa3f1820917b92da0142b755978f2
Joshua Burger <joshberger6969@outlook.com>
2022-06-22 21:05:02 -0400

    Import bootstrap and useState, useEffect

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+import React, { useState, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
+  
+
+
+
+
   return (
     <div className="App">
       <h1>Hello World!</h1>
