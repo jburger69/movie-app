97c4f2ed5dd79ee9a5c50c94020c23809d971732
Joshua Burger <joshberger6969@outlook.com>
2022-06-22 21:42:34 -0400

    Import movie header and search bar component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import MovieList from './components/MovieList';
+import MovielistHeader from './components/MovielistHeader';
+import SearchBox from './components/SearchBox';
 
 function App() {
 
   const [movies, setMovies] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
 
 
 
@@ -13,6 +16,10 @@ function App() {
   return (
     <div className="App">
       <div className="container-fluid movie-app">
+        <div className='row d-flex align-items-center md-4 mt-4'>
+          <MovielistHeader heading="Movies"/>
+          <SearchBox searchValue={searchValue} setSearchValue={setSearchValue}/>
+        </div>
         <div className="row">
           <MovieList movies={movies}/>
         </div>
